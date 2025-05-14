e41c44aa76f0a6d389eca3518801c0f97d835314
yasuki0707 <yasu19840707@gmail.com>
2025-05-14 06:35:04 +0000

    Added s3 bucket module

--- main.tf
+++ main.tf
@@ -194,6 +194,7 @@ locals {
   private_key_filename = "${var.prefix}-ssh-key.pem"
 }
 
+# comment
 resource "aws_key_pair" "hashicat" {
   key_name   = local.private_key_filename
   public_key = tls_private_key.hashicat.public_key_openssh
